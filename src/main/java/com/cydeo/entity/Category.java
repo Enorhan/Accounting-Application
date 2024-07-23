3443b0584f2923c3674cd4c66953f050b4b1c4bf
Ardonita Krasniqi <ardonitta05@gmail.com>
2024-07-24 00:48:59 +0200

    JDGR11-8 added Category entity and DTO

--- src/main/java/com/cydeo/entity/Category.java
+++ src/main/java/com/cydeo/entity/Category.java
@@ -2,8 +2,15 @@ package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Category extends BaseEntity {
+    @Column(nullable = false)
+    public String description;
+
+    @ManyToOne
+    public Company company;
 }
