a92742166555694ad373bf6e87845c8cf308586b
Art Hoxha <arthoxha34@gmail.com>
2024-07-24 13:55:20 +0200

    fix: added @component on top of InvoiceProductDTOConverter

--- src/main/java/com/cydeo/converter/InvoiceProductDTOConverter.java
+++ src/main/java/com/cydeo/converter/InvoiceProductDTOConverter.java
@@ -2,9 +2,10 @@ package com.cydeo.converter;
 
 import com.cydeo.dto.InvoiceProductDto;
 import com.cydeo.service.InvoiceProductService;
-import com.cydeo.service.InvoiceService;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class InvoiceProductDTOConverter implements Converter<Long, InvoiceProductDto> {
     private final InvoiceProductService invoiceProductService;
 
