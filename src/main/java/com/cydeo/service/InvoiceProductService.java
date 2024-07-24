931dbbccb47cd8b7a21ac379a4b869ab34125a80
Art Hoxha <arthoxha34@gmail.com>
2024-07-24 13:56:01 +0200

    cleared unnecessary imports

--- src/main/java/com/cydeo/service/InvoiceProductService.java
+++ src/main/java/com/cydeo/service/InvoiceProductService.java
@@ -1,8 +1,6 @@
 package com.cydeo.service;
 
-import com.cydeo.dto.InvoiceDto;
 import com.cydeo.dto.InvoiceProductDto;
-import org.springframework.core.convert.converter.Converter;
 
 public interface InvoiceProductService {
     InvoiceProductDto findById(Long id);
--- src/main/java/com/cydeo/service/InvoiceService.java
+++ src/main/java/com/cydeo/service/InvoiceService.java
@@ -1,11 +1,9 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.InvoiceDto;
-import com.cydeo.entity.Invoice;
 import com.cydeo.enums.InvoiceType;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface InvoiceService {
     List<InvoiceDto> listAllInvoicesByType(InvoiceType invoiceType);
