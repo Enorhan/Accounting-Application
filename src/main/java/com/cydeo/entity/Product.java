ec9e1b7c9df966ca3a9e77a3099e6d36a0734508
Yasin K. <yasinufukkanlioglu@gmail.com>
2024-07-24 18:00:08 +0100

    JDGR11-9 ProductEntity, ProductDTO, ProductUnit Enum

--- src/main/java/com/cydeo/entity/Product.java
+++ src/main/java/com/cydeo/entity/Product.java
@@ -1,9 +1,30 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import com.cydeo.enums.ProductUnit;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "products")
+@Getter
+@Setter
 public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int quantityInStock;
+    private int lowLimitAlert;
+
+    @Enumerated(EnumType.STRING)
+    private ProductUnit productUnit;
+
+    @ManyToOne
+    @JoinColumn(name = "category")
+    private Category category;
 }
