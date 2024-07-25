dfa18e59d3d6c2a2452680b1bee253b170df2cf6
Ardonita Krasniqi <ardonitta05@gmail.com>
2024-07-25 17:03:43 +0200

    JDGR11-19 added Category resources

--- src/main/java/com/cydeo/service/CategoryService.java
+++ src/main/java/com/cydeo/service/CategoryService.java
@@ -5,4 +5,6 @@ import java.util.List;
 
 public interface CategoryService {
     List<CategoryDto> listAllCategories();
+
+    CategoryDto findById(Long id);
 }
--- src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -1,13 +1,16 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.CategoryDto;
+import com.cydeo.entity.Category;
 import com.cydeo.repository.CategoryRepository;
 import com.cydeo.service.CategoryService;
 import com.cydeo.util.MapperUtil;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
     private final MapperUtil mapperUtil;
@@ -18,8 +21,15 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     public List<CategoryDto> listAllCategories(){
-        return categoryRepository.listAllCategories().stream()
+        return categoryRepository.findAll().stream()
                 .map(category -> mapperUtil.convert(category, new CategoryDto()))
                 .collect(Collectors.toList());
+    }
+
+    public CategoryDto findById(Long id){
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        return mapperUtil.convert(category, new CategoryDto());
     }
 }
