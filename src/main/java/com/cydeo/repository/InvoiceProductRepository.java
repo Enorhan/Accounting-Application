110bad9e03c03e9447925989af1dae3f0def7955
orhan-77 <herooneneverlast@gmail.com>
2024-07-26 18:35:08 +0300

    JDGR11-62- Dashboard Last 3 approved transaction

--- src/main/java/com/cydeo/repository/InvoiceProductRepository.java
+++ src/main/java/com/cydeo/repository/InvoiceProductRepository.java
@@ -2,9 +2,16 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Invoice;
 import com.cydeo.entity.InvoiceProduct;
+import com.cydeo.enums.InvoiceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InvoiceProductRepository  extends JpaRepository<InvoiceProduct,Long> {
+    @Query(value = "select a.* from invoice_products a join invoices b on a.invoice_id=b.id where b.invoice_status='APPROVED' and b.company_id= :companyId order by b.date desc limit 3", nativeQuery = true)
+    List<InvoiceProduct> find3LastApprovedTransactionDesc(@Param("companyId") Long companyId);
 }
