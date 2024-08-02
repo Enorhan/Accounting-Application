af136364391c043188e0ebcf07c12988cf19c98b
martinasenov <martin.manuilov.asenov@gmail.com>
2024-08-02 16:35:16 +0200

    Conflicts are resolved

--- src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -2,17 +2,15 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.CategoryDto;
 import com.cydeo.entity.Category;
-import com.cydeo.entity.Company;
 import com.cydeo.repository.CategoryRepository;
 import com.cydeo.service.CategoryService;
 import com.cydeo.service.CompanyService;
 import com.cydeo.util.MapperUtil;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+import java.util.ArrayList;
 @Service
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
@@ -24,25 +22,29 @@ public class CategoryServiceImpl implements CategoryService {
         this.companyService = companyService;
         this.mapperUtil = mapperUtil;
     }
-
     public List<CategoryDto> listAllCategories(){
         return categoryRepository.findAll().stream()
                 .map(category -> mapperUtil.convert(category, new CategoryDto()))
                 .collect(Collectors.toList());
     }
-
     public CategoryDto findById(Long id){
         Category category = categoryRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Category not found"));
 
         return mapperUtil.convert(category, new CategoryDto());
     }
+    public CategoryDto saveCategory(CategoryDto category){
+        Long companyId = companyService.getCompanyIdByLoggedInUser();
 
-    public CategoryDto saveCategory(Category category){
-        Long id = companyService.getCompanyIdByLoggedInUser();
-        category.setCompany(mapperUtil.convert(companyService.findById(id), new Company()));
+        Category existingCategory = categoryRepository.findByDescriptionAndCompanyId(category.getDescription(), companyId);
 
-        return mapperUtil.convert(categoryRepository.save(category), new CategoryDto());
+        if(existingCategory != null){
+            throw new IllegalArgumentException("Category already exists in Company.");
+        }
+
+        category.setCompany(companyService.findById(companyId));
+
+        return mapperUtil.convert(categoryRepository.save(mapperUtil.convert(category, new Category())), new CategoryDto());
     }
 
     @Override
