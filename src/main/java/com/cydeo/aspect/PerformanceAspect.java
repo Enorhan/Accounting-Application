f78e1e1f99d2a2fc09a140a263ab405e5e91a5a5
Art Hoxha <arthoxha34@gmail.com>
2024-08-02 13:12:51 +0200

    refactor

--- src/main/java/com/cydeo/aspect/PerformanceAspect.java
+++ src/main/java/com/cydeo/aspect/PerformanceAspect.java
@@ -11,14 +11,12 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class PerformanceAspect {
-
     @Pointcut("@annotation(com.cydeo.annotation.ExecutionTime)")
     public void executionTimePC() {
     }
 
     @Around("executionTimePC()")
     public Object aroundAnyExecutionTimeAdvice(ProceedingJoinPoint proceedingJoinPoint) {
-
         long beforeTime = System.currentTimeMillis();
         Object result = null;
         log.info("Execution starts:");
@@ -37,7 +35,5 @@ public class PerformanceAspect {
         );
 
         return result;
-
     }
-
 }
