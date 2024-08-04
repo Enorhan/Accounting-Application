f49209fc0e3776c54d6bd75554011a49213293da
orhan-77 <herooneneverlast@gmail.com>
2024-08-04 14:40:59 +0300

    JDGR11-71 log runtime exception

--- src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -1,6 +1,8 @@
 package com.cydeo.aspect;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -8,4 +10,13 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class LoggingAspect {
+
+    @AfterThrowing(pointcut = "execution(* com.cydeo..*(..))", throwing = "ex")
+    public void logRuntimeException(JoinPoint joinPoint, RuntimeException ex) {
+        String methodName = joinPoint.getSignature().getName();
+        String exceptionName = ex.getClass().getSimpleName();
+        String exceptionMessage = ex.getMessage();
+
+        log.error("Exception in method '{}': {} - {}", methodName, exceptionName, exceptionMessage);
+    }
 }
