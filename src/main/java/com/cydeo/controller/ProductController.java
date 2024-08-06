b5efff4d541ab3f27e0a12e8e89835d990c2f34e
martinasenov <martin.manuilov.asenov@gmail.com>
2024-08-06 23:03:47 +0200

    the order of bindingResult statements in the @PostMapping("/create") and @PostMapping("/update/{productId}") has been changed.

--- src/main/java/com/cydeo/controller/ProductController.java
+++ src/main/java/com/cydeo/controller/ProductController.java
@@ -45,14 +45,12 @@ public class ProductController {
     @PostMapping("/create")
     public String insertProduct(@Valid @ModelAttribute("newProduct") ProductDto productDto,
                                 BindingResult bindingResult, Model model) {
-
-        productService.checkIfProductNameAlreadyExists(productDto.getName(), bindingResult);
-
         if (bindingResult.hasErrors()) {
             model.addAttribute("categories", categoryService.listAllCategoriesByCompany());
             model.addAttribute("productUnits", Arrays.asList(ProductUnit.values()));
             return "product/product-create";
         }
+        productService.checkIfProductNameAlreadyExists(productDto.getName(), bindingResult);
         productService.save(productDto);
         return "redirect:/products/list";
     }
@@ -71,19 +69,14 @@ public class ProductController {
     public String updateProduct(@PathVariable("productId") Long productId,
                                 @Valid @ModelAttribute("product") ProductDto productDto,
                                 BindingResult bindingResult, Model model) {
-
         productDto.setId(productId);
-
-        productService.checkIfProductNameAlreadyExists(productDto.getName(), bindingResult);
-
         if (bindingResult.hasErrors()) {
             model.addAttribute("categories", categoryService.listAllCategoriesByCompany());
             model.addAttribute("productUnits", Arrays.asList(ProductUnit.values()));
             return "product/product-update";
         }
-
+        productService.checkIfProductNameAlreadyExists(productDto.getName(), bindingResult);
         productService.update(productDto, productId);
-
         return "redirect:/products/list";
     }
 
