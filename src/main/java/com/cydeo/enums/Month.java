f11964873a8923a87add74e91f96cb7462c47543
Enesorhan <enestcp@gmail.com>
2024-08-07 21:24:21 +0200

    admin can now see subscription fee list

--- src/main/java/com/cydeo/enums/Month.java
+++ src/main/java/com/cydeo/enums/Month.java
@@ -2,9 +2,13 @@ package com.cydeo.enums;
 
 public enum Month {
     JANUARY("January"),FEBRUARY("February"),MARCH("March"),APRIL("April"),MAY("May"),JUNE("June"),JULY("July"),AUGUST("August"),SEPTEMBER("September"),OCTOBER("October"),NOVEMBER("November"),DECEMBER("December");
-    private String value;
+    private final String value;
 
     Month(String value) {
         this.value = value;
+    }
+
+    public String getValue() {
+        return value;
     }
 }
--- src/main/java/com/cydeo/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/PaymentServiceImpl.java
@@ -1,9 +1,81 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.PaymentDto;
+import com.cydeo.entity.Payment;
+import com.cydeo.enums.Month;
+import com.cydeo.repository.PaymentRepository;
 import com.cydeo.service.PaymentService;
+import com.cydeo.util.MapperUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+
+    private final PaymentRepository paymentRepository;
+    private final MapperUtil mapperUtil;
+
+    public PaymentServiceImpl(PaymentRepository paymentRepository, MapperUtil mapperUtil) {
+        this.paymentRepository = paymentRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+    private static final BigDecimal FIXED_FEE = BigDecimal.valueOf(250);
+
+
+    @Override
+    public List<PaymentDto> getPaymentForYear(int year) {
+
+        List<Payment> payments = paymentRepository.findByYear(year);
+
+
+        if (payments.isEmpty()) {
+            createPaymentsForYear(year);
+            payments = paymentRepository.findByYear(year);
+        }
+        return payments.stream()
+                .sorted(Comparator.comparing(Payment::getMonth))
+                .map(payment -> mapperUtil.convert(payment, new PaymentDto()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void createPaymentsForYear(int year) {
+        for (Month month : Month.values()){
+            if (paymentRepository.findByYearAndAndMonth(year, month) == null){
+                Payment payment = new Payment();
+                payment.setYear(year);
+                payment.setMonth(month);
+                payment.setAmount(FIXED_FEE);
+                payment.setPaid(false);
+                paymentRepository.save(payment);
+            }
+        }
+    }
+
+    @Override
+    public void processPayment(Long paymentId) {
+        Payment payment = paymentRepository.findById(paymentId)
+                .orElseThrow(() -> new RuntimeException("Paymment not found with id " + paymentId));
+        payment.setPaid(true);
+        payment.setPaymentDate(LocalDate.now());
+        paymentRepository.save(payment);
+    }
+
+    @Override
+    public PaymentDto getInvoice(Long id) {
+        Payment payment = paymentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Payment not found with id: " + id));
+
+        return mapperUtil.convert(payment, new PaymentDto());
+    }
+
 }
